44230097873979c636c39e6b9be99e169e6ee46c
Your Name <you@example.com>
2025-02-08 16:28:58 +0500

    Project setup done

--- Authentication/main.js
+++ Authentication/main.js
@@ -1,48 +1,54 @@
 import { supabase } from "../hide.js";
 
-// console.log(supabase);
-// signup functions
 let signup_Btn = document.getElementById("signup");
-signup_Btn.addEventListener("click", async (data) => {
-  let signupemail = document.getElementById("Email");
-  let signupname = document.getElementById("name");
-  let signuppass = document.getElementById("pass");
-
-  console.log(signupemail.value);
-  console.log(signupname.value);
-  console.log(signuppass.value);
+signup_Btn.addEventListener("click", async () => {
+  let signupemail = document.getElementById("Email").value;
+  let signupname = document.getElementById("name").value;
+  let signuppass = document.getElementById("pass").value;
 
   try {
     const { data, error } = await supabase.auth.signUp({
-      name: signupname.value,
-      email: signupemail.value,
-      password: signuppass.value,
+      email: signupemail,
+      password: signuppass,
+      options: {
+        data: { name: signupname } 
+      }
     });
-    console.log("Successfully signed up", data);
 
     if (error) {
-      console.log(error);
+      console.error("Signup Error:", error.message);
+    } else {
+      console.log("Signup Success:", data);
     }
-  } catch (error) {
-    console.log(err);
+  } catch (err) {
+    console.error("Unexpected Error:", err);
   }
 
-  signupemail.value = "";
-  signupname.value = "";
-  signuppass.value = "";
+  document.getElementById("Email").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("pass").value = "";
 });
 
 
-
-
-
-// // login Function
-// let login_btn = document.getElementById("login");
-// login_btn.addEventListener("click", () => {
-//   // console.log("hello wprld ");
-
-//   let loginemail = document.getElementById("Email_login");
-//   let loginpass = document.getElementById("pass_login");
-//   console.log(loginemail);
-//   console.log(loginpass);
-// });
+ // login Function
+let login_btn = document.getElementById("login");
+login_btn.addEventListener("click", async () => {
+  
+  let loginemail = document.getElementById("Email_login");
+  let loginpass = document.getElementById("pass_login");
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: loginemail.value ,
+      password: loginpass.value,
+    })
+    if (error) {
+      console.error("Signup Error:", error.message);
+    } else {
+      console.log("Hello user",);
+    }
+  }catch (err) {
+    console.error("Unexpected Error:", err);
+  }
+  document.getElementById("Email_login").value = ""
+   document.getElementById("pass_login").value = ""
+});
