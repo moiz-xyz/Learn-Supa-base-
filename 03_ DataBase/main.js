95fac1e0596a609b826e150c80cd7c883df27383
Your Name <you@example.com>
2025-02-10 15:07:54 +0500

    database inserted

---
+++ 03_ DataBase/main.js
@@ -0,0 +1,28 @@
+import { supabase } from "../hide.js";
+// console.log(supabase);
+
+
+let signup_Btn = document.getElementById("signup");
+signup_Btn.addEventListener("click", async () => {
+  let signupemail = document.getElementById("Email").value;
+  let signupname = document.getElementById("name").value;
+  let signuppass = document.getElementById("pass");
+
+  try {
+    const { error } = await supabase
+  .from('Practise')
+  .insert({ 
+     Name: signupname ,
+     Email : signupemail ,
+     })
+     if (error) {
+        console.log(error.message);
+     } else{
+console.log("Data done ");
+        
+     }
+
+  } catch (err) {
+    console.log("unexpected eror" + err.message);
+  }
+})
